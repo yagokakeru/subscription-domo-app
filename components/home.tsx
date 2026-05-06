e7ec1943b6862e3f4a165e75e5f9cc8e3c6a9851
p01nt-yago <yagou@p01nt.com>
2026-05-06 18:18:51 +0900

    bottonコンポーネント作成

--- components/home.tsx
+++ components/home.tsx
@@ -1,9 +1,36 @@
+'use client'
+
+import { Button } from './ui/button'
+import Link from 'next/link'
+
 export default function Home() {
     return (
-        <p className="text-3xl font-bold leading-loose text-center">
-            プロンプターアプリを使って
-            <br />
-            カメラから目を離さず話そう👀
-        </p>
+        <>
+            <p className="text-3xl font-bold leading-loose text-center">
+                プロンプターアプリを使って
+                <br />
+                カメラから目を離さず話そう👀
+            </p>
+            <div>
+                <Button asChild>
+                    <Link href="/sign-up">無料で始める</Link>
+                </Button>
+            </div>
+            <div>
+                <Button asChild size="lg">
+                    <Link href="/sign-up">無料で始める</Link>
+                </Button>
+            </div>
+            <div>
+                <Button asChild variant="secondary" size="sm">
+                    <Link href="/sign-up">無料で始める</Link>
+                </Button>
+            </div>
+            <div>
+                <Button asChild variant="ghost">
+                    <Link href="/sign-up">無料で始める</Link>
+                </Button>
+            </div>
+        </>
     )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,8 @@
 import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+// import { twMerge } from 'tailwind-merge'
 
-// 条件付きの className をまとめ、Tailwind CSS の競合するクラスを解決します。
+// 条件付きの className をまとめるクラスヘルパー
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
+    // return twMerge(clsx(inputs))
+    return clsx(inputs)
 }
