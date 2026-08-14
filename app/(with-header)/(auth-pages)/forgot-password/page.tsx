d25eab2eae2aec044e2852d95ad84e300fd3e68b
p01nt-yago <yagou@p01nt.com>
2026-08-14 14:48:59 +0900

    feature:パスワード忘れページのエラーメッセージ実装

--- app/(with-header)/(auth-pages)/forgot-password/page.tsx
+++ app/(with-header)/(auth-pages)/forgot-password/page.tsx
@@ -1,10 +1,5 @@
-import { Message } from '@/components/form-message'
 import ForgotPasswordForm from '@/components/app/auth-pages/forgot-password'
 
-export default async function ForgotPassword(props: {
-    searchParams: Promise<Message>
-}) {
-    const searchParams = await props.searchParams
-
-    return <ForgotPasswordForm message={searchParams} />
+export default async function ForgotPassword() {
+    return <ForgotPasswordForm />
 }
